e932b98b0a6ff67cb9749ec7b59ef1992b78e4d7
Haiko Schol <hs@haikoschol.com>
2024-01-02 10:21:17 +0700

    remove dead code

--- photos/urls.py
+++ photos/urls.py
@@ -11,8 +11,4 @@ urlpatterns = [
     path('upload', views.upload, name='upload'),
     path('create_upload_url', views.create_upload_url, name='create_upload_url'),
     path('add_photo', views.add_photo, name='add_photo'),
-
-    # TODO add converter to float
-    # https://docs.djangoproject.com/en/5.0/topics/http/urls/#registering-custom-path-converters
-    # path('location/<latitude>/<longitude>', views.location, name='location'),
 ]
